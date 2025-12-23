0483ca0b391707c185b44bda1c4f1560dfd9ccec
MahderK <mahderkassaw@gmail.com>
2025-12-23 18:24:48 +0300

    Added id and quantity to Drug node

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main()
 {
     DrugBST dr;
     PatientQueue pq;
-    dr.addDrug("iboprofien");
-    dr.addDrug("paracetamol");
+    dr.addDrug("iboprofien", 01, 10);
+    dr.addDrug("paracetamol", 02, 20);
     dr.displayDrugs();
 }
--- modules/DrugBST.cpp
+++ modules/DrugBST.cpp
@@ -3,33 +3,45 @@
 using namespace std;
 
 // Drug constructor
-Drug::Drug(string n) : name(n), left(nullptr), right(nullptr) {}
+Drug::Drug(string n, int i, int quan) : name(n), id(i), quantity(quan), left(nullptr), right(nullptr) {}
 
 // DrugBST constructor
 DrugBST::DrugBST() : root(nullptr) {}
 
 // Insert into BST
-Drug *DrugBST::insert(Drug *node, string name)
+Drug *DrugBST::insert(Drug *node, string name, int id, int quantity)
 {
     if (!node)
-        return new Drug(name);
+        return new Drug(name, id, quantity);
     if (name < node->name)
-        node->left = insert(node->left, name);
+        node->left = insert(node->left, name, id, quantity);
     else if (name > node->name)
-        node->right = insert(node->right, name);
+        node->right = insert(node->right, name, id, quantity);
     return node;
 }
 
-// Search in BST
-bool DrugBST::search(Drug *node, string name)
+// Search by name in BST
+bool DrugBST::searchByName(Drug *node, string name)
 {
     if (!node)
         return false;
     if (node->name == name)
         return true;
     if (name < node->name)
-        return search(node->left, name);
-    return search(node->right, name);
+        return searchByName(node->left, name);
+    return searchByName(node->right, name);
+}
+
+// Search by id in BST
+bool DrugBST::searchById(Drug *node, int id)
+{
+    if (!node)
+        return false;
+    if (node->id == id)
+        return true;
+    if (id < node->id)
+        return searchById(node->left, id);
+    return searchById(node->right, id);
 }
 
 // In-order traversal
@@ -38,20 +50,25 @@ void DrugBST::inorder(Drug *node)
     if (!node)
         return;
     inorder(node->left);
-    cout << node->name << endl;
+    cout << node->name << ", " << node->id << ", " << node->quantity << endl;
     inorder(node->right);
 }
 
 // Public methods
-void DrugBST::addDrug(string name)
+void DrugBST::addDrug(string name, int id, int quantity)
 {
-    root = insert(root, name);
+    root = insert(root, name, id, quantity);
 }
 
-void DrugBST::findDrug(string name)
+void DrugBST::findDrugName(string name)
 {
     cout << "Searching for " << name << ": "
-         << (search(root, name) ? "Found" : "Not Found") << endl;
+         << (searchByName(root, name) ? "Found" : "Not Found") << endl;
+}
+
+void DrugBST::findDrugId(int id)
+{
+    
 }
 
 void DrugBST::displayDrugs()
